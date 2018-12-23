cd57a93b80990ebb8843f56e470409ade9888ac6
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2018-12-23 13:18:16 -0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+//===- index.ts - Main file ------------------------------------------------===//
+//
+// Copyright (c) 2018 J. Morgan Lieberthal
+// Licensed under the MIT License
+//
+//===-----------------------------------------------------------------------===//
+
+import "./styles.scss";
+
+export function hello(name: string) {
+  console.log(`Hello, ${name}!`);
+}
