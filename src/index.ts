bc4d137b53eb414502620c27ec38ea19aa98a21c
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2018-12-30 13:18:24 -0700

    move dead files away

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-//===- index.ts - Main file ------------------------------------------------===//
-//
-// Copyright (c) 2018 J. Morgan Lieberthal
-// Licensed under the MIT License
-//
-//===-----------------------------------------------------------------------===//
-
-import "./app/app.main";
