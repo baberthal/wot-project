7cbbaa048ec2cec6fd30b243774b202dffaa36b0
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2018-12-30 13:18:24 -0700

    remove dead code

--- server/src/models/mixins/named.ts
+++
@@ -1,6 +0,0 @@
-//===- models/mixins/named.ts - Named Mixin --------------------------------===//
-//
-// Copyright (c) 2019 J. Morgan Lieberthal
-// Licensed under the MIT License
-//
-//===-----------------------------------------------------------------------===//
