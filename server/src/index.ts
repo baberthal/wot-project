b44195425e218c3be39ce6767c92b93b1ee857dd
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2018-12-30 13:18:24 -0700

    clean up main file

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,18 +5,25 @@
 //
 //===-----------------------------------------------------------------------===//
 
-import app from "./servers/http";
+import createApp from "./servers/http";
+import { isDevMode } from "./util/dev-mode";
 // import logger from "./util/logger";
 // import { WebSocketServer } from "./servers/ws_server";
 
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 async function main() {
+  const app = createApp();
+
   app.listen(port, "0.0.0.0", (err, address) => {
     if (err) {
       app.log.error(err);
     }
     app.log.info(`app is up and running on ${address}`);
+
+    if (isDevMode()) {
+      app.log.info("### Routes:", app.printRoutes(), "###");
+    }
   });
 }
 
