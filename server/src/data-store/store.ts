1d5bc4162c7c2812b73ce9e9a90c376305cf9a85
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2018-12-30 13:18:24 -0700

    remove dead code

--- server/src/data-store/store.ts
+++
@@ -1,9 +0,0 @@
-//===- data-store/store.ts - Data Store Base Class -------------------------===//
-//
-// Copyright (c) 2019 J. Morgan Lieberthal
-// Licensed under the MIT License
-//
-//===-----------------------------------------------------------------------===//
-
-export class Store {
-}
