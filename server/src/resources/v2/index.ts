6f48b340e0a20e9ad2b15eb63a863221a013c2dc
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2018-12-30 13:18:24 -0700

    finish resources clean-up

--- server/src/resources/v2/index.ts
+++
@@ -1,8 +0,0 @@
-//===- resources/v2/index.ts -----------------------------------------------===//
-//
-// Copyright (c) 2019 J. Morgan Lieberthal
-// Licensed under the MIT License
-//
-//===-----------------------------------------------------------------------===//
-
-export const UNUSED = 0;
