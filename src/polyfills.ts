80cda08b90ee4c2ca530648f5df148e69bd0b9c9
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2019-01-03 17:22:47 -0700

    edit polyfills

---
+++ src/polyfills.ts
@@ -0,0 +1,6 @@
+//===- polyfills.ts - Polyfills for older browsers -------------------------===//
+//
+// Copyright (c) 2018 J. Morgan Lieberthal
+// Licensed under the MIT License
+//
+//===-----------------------------------------------------------------------===//
