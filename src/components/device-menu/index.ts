c46066597a6ae513365148ba2d31fae5bb475ea6
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2019-01-03 17:23:19 -0700

    add device-menu component

---
+++ src/components/device-menu/index.ts
@@ -0,0 +1,8 @@
+//===- components/device-menu/index.ts - Device Menu -----------------------===//
+//
+// Copyright (c) 2018 J. Morgan Lieberthal
+// Licensed under the MIT License
+//
+//===-----------------------------------------------------------------------===//
+
+export * from "./device-menu.component";
