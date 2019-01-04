2b29dbef178adfc5db38a9b286ae8fbd0fecc27d
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2019-01-03 17:27:39 -0700

    add bp data store module

---
+++ src/data-store/store.ts
@@ -0,0 +1,9 @@
+//===- data-store/store.ts - Data Store Base Class -------------------------===//
+//
+// Copyright (c) 2019 J. Morgan Lieberthal
+// Licensed under the MIT License
+//
+//===-----------------------------------------------------------------------===//
+
+export class Store {
+}
