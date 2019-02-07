9b61b73c91450e02dc4ace0624ec58cb09cd814f
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2019-02-07 11:31:39 -0700

    Fix reference error

--- server/src/resources/config.ts
+++ server/src/resources/config.ts
@@ -15,86 +15,6 @@ import {
   ISensorConfig
 } from "./interfaces";
 
-export class ResourcesConfig {
-  static Default: ResourcesConfig = new ResourcesConfig(defaultConfig);
-
-  readonly devices: { [id: string]: DeviceConfig } = {};
-
-  /**
-   * Load a ResourcesConfig from a given path or file descriptor.
-   *
-   * @param path The path of the file, or an open file descriptor
-   */
-  static load(path: string | number): Promise<ResourcesConfig> {
-    return new Promise((resolve, reject) => {
-      fs.readFile(path, "utf8", (err, data) => {
-        if (err) reject(err);
-
-        try {
-          const json = JSON.parse(data);
-          const config = new ResourcesConfig(json);
-          resolve(config);
-        } catch (e) {
-          reject(e);
-        }
-      });
-    });
-  }
-
-  constructor(config?: IResourcesConfig) {
-    if (config) {
-      for (let i = 0, keys = Object.keys(config); i < keys.length; i++) {
-        const key = keys[i];
-        this.devices[key] = new DeviceConfig(key, config[key]);
-      }
-    } else {
-      this.devices = Object.assign({}, ResourcesConfig.Default.devices);
-    }
-  }
-}
-
-export class DeviceConfig {
-  /** Unique ID of this device (machine-readable). */
-  readonly id: string;
-
-  /** Name of the device (human-readable). */
-  readonly name: string;
-
-  /** Human-readable description for this device. */
-  readonly description: string;
-
-  /** Port on which this device is listening. */
-  readonly port: number;
-
-  /** Configurations for attached sensors, by id. */
-  readonly sensors: { [id: string]: SensorConfig } = {};
-
-  /** Configurations for attached actuators, by group by id. */
-  readonly actuators: {
-    [group: string]: {
-      [id: string]: ActuatorConfig;
-    };
-  } = {};
-
-  constructor(id: string, config: IDeviceConfig) {
-    this.id = id;
-    this.name = config.name;
-    this.description = config.description;
-    this.port = config.port;
-
-    const sensors = config.sensors;
-    for (let i = 0, keys = Object.keys(sensors); i < keys.length; i++) {
-      const key = keys[i];
-      this.sensors[key] = new SensorConfig(key, config.sensors[key]);
-    }
-
-    const actuators = config.actuators;
-    for (let i = 0, keys = Object.keys(actuators); i < keys.length; i++) {
-      const key = keys[i];
-    }
-  }
-}
-
 export class SensorConfig {
   /** Unique ID of this device (machine-readable). */
   readonly id: string;
@@ -146,5 +66,94 @@ export class ActuatorConfig {
     this.description = config.description;
     this.value = config.value;
     this.gpio = config.gpio;
+  }
+}
+
+export class DeviceConfig {
+  /** Unique ID of this device (machine-readable). */
+  readonly id: string;
+
+  /** Name of the device (human-readable). */
+  readonly name: string;
+
+  /** Human-readable description for this device. */
+  readonly description: string;
+
+  /** Port on which this device is listening. */
+  readonly port: number;
+
+  /** Configurations for attached sensors, by id. */
+  readonly sensors: { [id: string]: SensorConfig } = {};
+
+  /** Configurations for attached actuators, by group by id. */
+  readonly actuators: {
+    [group: string]: {
+      [id: string]: ActuatorConfig;
+    };
+  } = {};
+
+  constructor(id: string, config: IDeviceConfig) {
+    this.id = id;
+    this.name = config.name;
+    this.description = config.description;
+    this.port = config.port;
+
+    const sensors = config.sensors;
+    for (let i = 0, keys = Object.keys(sensors); i < keys.length; i++) {
+      const key = keys[i];
+      this.sensors[key] = new SensorConfig(key, config.sensors[key]);
+    }
+
+    const actuators = config.actuators;
+    for (let i = 0, keys = Object.keys(actuators); i < keys.length; i++) {
+      const groupName = keys[i];
+
+      // set up the 'group'
+      this.actuators[groupName] = this.actuators[groupName] || {};
+
+      const group = actuators[groupName];
+      for (let j = 0, keys2 = Object.keys(group); j < keys2.length; j++) {
+        const key = keys2[j];
+        this.actuators[groupName][key] = new ActuatorConfig(key, group[key]);
+      }
+    }
+  }
+}
+
+export class ResourcesConfig {
+  static Default: ResourcesConfig = new ResourcesConfig(defaultConfig);
+
+  readonly devices: { [id: string]: DeviceConfig } = {};
+
+  /**
+   * Load a ResourcesConfig from a given path or file descriptor.
+   *
+   * @param path The path of the file, or an open file descriptor
+   */
+  static load(path: string | number): Promise<ResourcesConfig> {
+    return new Promise((resolve, reject) => {
+      fs.readFile(path, "utf8", (err, data) => {
+        if (err) reject(err);
+
+        try {
+          const json = JSON.parse(data);
+          const config = new ResourcesConfig(json);
+          resolve(config);
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  }
+
+  constructor(config?: IResourcesConfig) {
+    if (config) {
+      for (let i = 0, keys = Object.keys(config); i < keys.length; i++) {
+        const key = keys[i];
+        this.devices[key] = new DeviceConfig(key, config[key]);
+      }
+    } else {
+      this.devices = Object.assign({}, ResourcesConfig.Default.devices);
+    }
   }
 }
