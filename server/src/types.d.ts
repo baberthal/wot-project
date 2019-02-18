4e1876ee3cc950013837c3caa03d1cf41ebd011e
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2019-02-18 13:29:13 -0700

    main type def file

--- server/src/types.d.ts
+++ server/src/types.d.ts
@@ -9,3 +9,10 @@ declare module "pigpio-mock" {
   import pigpio = require("pigpio");
   export = pigpio;
 }
+
+declare module "pino-caller" {
+  /* tslint:disable-next-line */
+  import pino = require("pino");
+  function callsite(p: pino.Logger): pino.Logger;
+  export = callsite;
+}
